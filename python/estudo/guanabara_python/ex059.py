b4eaf302374203873819403228e6a342ca238553
barradox <vinni_vmp@hotmail.com>
2026-03-22 15:12:01 -0300

    resolucao ex059

---
+++ python/estudo/guanabara_python/ex059.py
@@ -0,0 +1,52 @@
+"""
+Crie um programa que leia dois valores e mostre um menu na tela:
+
+[1] somar
+
+[2] multiplicar
+
+[3] maior
+
+[4] digitar novos números
+
+[5] sair do programa
+
+Seu programa deverá realizar a operação solicitada em cada caso.
+
+"""
+menu = False
+while not menu:
+    
+    n1 = float(input('Digite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+    opcao = int(input('''Escolha o que quer fazer:
+        [ 1 ] somar
+        [ 2 ] multiplicar
+        [ 3 ] maior
+        [ 4 ] inserir novos números
+        [ 5 ] sair do programa\n'''))
+    if opcao == 1:
+        soma = n1 + n2
+        print('A soma de {} com {} é {}'.format(n1, n2, soma))
+
+    elif opcao == 2:
+        mult = n1 * n2
+        print('A multiplicação entre {} e {} é {}'.format(n1, n2, mult))
+
+    elif opcao == 3:
+        if n1 > n2:
+            print('O maior número é {}'.format(n1))
+        elif n1 < n2:
+            print('O maior número é {}'.format(n2))
+        else:
+            print('Os números são iguais.')
+    
+    elif opcao == 4:
+       None
+
+    elif opcao == 5:
+        break
+
+    else: 
+        print('Opção inválida.')
+    print('-=' * 13)
