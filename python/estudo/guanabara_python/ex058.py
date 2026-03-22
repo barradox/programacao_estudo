52dc50e97dd59bcf7597973e05d30248827cb097
barradox <vinni_vmp@hotmail.com>
2026-03-22 13:06:18 -0300

    correção ex058

--- python/estudo/guanabara_python/ex058.py
+++ python/estudo/guanabara_python/ex058.py
@@ -6,19 +6,44 @@ para vencer.
 
 """
 
+# from random import randint
+
+# print('Vou pensar em um número entre 0 e 10.')
+# numero = int(input('Tente adivinhar o número que eu pensei: '))
+
+# computador = randint(0, 10)
+# tentativa = 1
+# while numero  != computador:
+#     if numero < computador:
+#         print ('Mais...')
+#     else:
+#         print ('Menos...')
+#     numero = int(input('Tente novamente '))
+#     tentativa += 1
+
+# print('Parabéns, você ACERTOU em {} tentativas. '.format(tentativa))
+
+# Correção
+
 from random import randint
 
-print('Vou pensar em um número entre 0 e 10.')
-numero = int(input('Tente adivinhar o número que eu pensei: '))
-
 computador = randint(0, 10)
-tentativa = 1
-while numero  != computador:
-    if numero < computador:
-        print ('Mais...')
-    else:
-        print ('Menos...')
-    numero = int(input('Tente novamente '))
-    tentativa += 1
 
-print('Parabéns, você ACERTOU em {} tentativas. '.format(tentativa))
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi? ')
+
+acertou = False
+palpite = 0
+
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpite += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+
+print('Acertou com {} tentativas. Parabéns! '.format(palpite))
